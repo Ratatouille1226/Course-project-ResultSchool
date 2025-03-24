d399245a9afb52900ffc608d71dd021b3067b283
Andrey <kotlyar.andrey03@mail.ru>
2025-03-25 00:17:24 +0300

    Сделал футер, добавил API погоды

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,51 @@
+import { useEffect, useState } from 'react';
+import styled from 'styled-components';
+
+const FooterContainer = ({ className }) => {
+	const [city, setCity] = useState('');
+	const [temperature, setTemperature] = useState('');
+	const [weather, setWeather] = useState('');
+
+	useEffect(() => {
+		fetch(
+			'https://api.openweathermap.org/data/2.5/weather?q=Moscow&units=metric&lang=ru&appid=699abd7bdc958c4f820cfea1da62167e',
+		)
+			.then((res) => res.json())
+			.then(({ name, main, weather }) => {
+				setCity(name);
+				setTemperature(Math.round(main.temp));
+				setWeather(weather[0].description);
+			});
+	}, []);
+
+	return (
+		<div className={className}>
+			<div>
+				<div>Блог веб-разработчика</div>
+				<div>frontend@mail.ru</div>
+			</div>
+			<div>
+				<div>
+					{city},{' '}
+					{new Date().toLocaleString('ru', { day: 'numeric', month: 'long' })}
+				</div>
+				<div>
+					{temperature} градусов, {weather}
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export const Footer = styled(FooterContainer)`
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
+	padding: 0px 50px;
+	text-align: center;
+	height: 100px;
+	width: 1000px;
+	background-color: #240090;
+	box-shadow: 0px 0px 20px #0c0032;
+	color: white;
+`;
--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,2 +1,3 @@
 export * from './header/Header';
 export * from './icon-awesome/Icon';
+export * from './footer/Footer';
